7b0e7178fce0eeacf4ff52bb80fc2b1a8075fdd0
amirhossein laptop <amirhosseinbabaali83@gmail.com>
2026-08-08 11:29:56 +0330

    add string methodes

--- project/HelloWorld/SignUp/Program.cs
+++ project/HelloWorld/SignUp/Program.cs
@@ -10,11 +10,12 @@
             Console.ForegroundColor = ConsoleColor.Gray;//Set Text color
             Console.Clear();// fix 
             Console.Title = "Hello World portal!";//Title
-            Console.SetCursorPosition(3, 3);//Start of cursor
+            Console.SetCursorPosition(3, 3);//ser cursou posistion 
             // Configure console appearance
             // get first name
             Console.WriteLine("welcome! please enter your first name");
-            string? firstName = Console.ReadLine();
+            string firstName = Console.ReadLine() ?? "";
+            firstName = firstName.Trim().ToLower();
             if (string.IsNullOrWhiteSpace(firstName))
             {
                 Console.WriteLine("name is necessary");
@@ -22,7 +23,9 @@
             }
             // get last name    
             Console.WriteLine("now enter your last name");
-            string? lastName = Console.ReadLine();
+            string? lastName = Console.ReadLine() ?? "";
+            lastName = lastName.Trim();
+            lastName = lastName.ToLower();
             if (string.IsNullOrWhiteSpace(lastName))
             {
                 Console.WriteLine("Last name is required");
@@ -30,7 +33,8 @@
             }
             // get age
             Console.WriteLine("how old are you ?");
-            string ageStr = Console.ReadLine() ?? "0";// we can do try parse 
+            string ageStr = Console.ReadLine() ?? "0";// we should do try parse 
+            ageStr = ageStr.Trim();
             bool isAgeValid = byte.TryParse(ageStr, out byte age);
             if (!isAgeValid || age < 1 || age > 120)
             {
@@ -45,15 +49,17 @@
             // get gender
             //string gender;
             Console.WriteLine("enter gender--> m: Male , f: Female , e:Email :/");
-            string? gender = Console.ReadLine();
+            string? gender = Console.ReadLine() ?? "";
+            gender = gender.Trim();
+            gender = gender.ToLower();
             switch(gender)
             {
-                case "m" or "M" or "male" or "Male" :
+                case "m" or "male":
                     {
                         gender = "Male";
                         break;
                     }
-                case "f" or "F" or "female" or "Female":
+                case "f" or "female" :
                     {
                         gender = "Female";
                         break;
@@ -75,11 +81,12 @@
                     }
             }
             // print profile
-            Console.WriteLine( $"Registration Summary:\n" +
-                               $"first name: {firstName}\n" +
-                               $"last name: {lastName}\n"+
-                               $"age: {age}\n"+
-                               $"gender: {gender}");
+            Console.WriteLine(
+                $"Registration Summary:\n" +
+                $"first name: {firstName}\n" +
+                $"last name: {lastName}\n"+
+                $"age: {age}\n"+
+                $"gender: {gender}");
         }
     }
 }
