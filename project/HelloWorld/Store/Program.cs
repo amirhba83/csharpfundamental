b9c3558dd908f7a9556d62de277aa23ed766a187
elia <amirhosseinbabaali83@gmail.com>
2026-08-18 17:20:50 +0330

    add store project to practice oop fundamentals

---
+++ project/HelloWorld/Store/Program.cs
@@ -0,0 +1,22 @@
+﻿using Store.Models;
+
+namespace Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("mobile");
+            Product p2 = new Product("mobile",100);
+            Product p3 = new Product("mobile") {Name="minote"};
+            p1.ApplyDiscount(10);
+            p2.ApplyDiscountByAmount(10);
+            p1.Show();
+            p2.Show();
+            p3.Show();
+            Product.ShowTotalProduct();
+
+
+        }
+    }
+}
