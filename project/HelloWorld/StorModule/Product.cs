55e2764226189cb7c129f5e036c6994ab9115bc8
elia <amirhosseinbabaali83@gmail.com>
2026-09-01 14:53:23 +0330

    add dll project for practice

---
+++ project/HelloWorld/StorModule/Product.cs
@@ -0,0 +1,72 @@
+﻿namespace Store.Models;
+
+public class Product
+{
+    //Fields
+    private decimal _price;
+    private readonly int _id = default;
+    private static int _nextId;
+    private static int _count;
+    //Properties
+    public decimal Price
+    {
+        get { return _price; }
+        private set
+        {
+            if (value > 0)
+                _price = value;
+        }
+    }
+
+    public string Name { get; set; }
+    public int Id
+    {
+        get { return _id; }
+    }
+    //Constructor
+    public Product(string name, decimal price)
+    {
+        this.Name = name;
+        this.Price = price;
+        _id = _nextId;
+        _nextId++;
+        _count++;
+    }
+    public Product(string name) : this(name: name, price: 60)
+    {
+    }
+    // Methodes 
+    public void ApplyDiscount(decimal percent)
+    {
+        this.Price -= (this.Price * percent) / 100;
+    }
+    public void ApplyDiscountByAmount(decimal amount)
+    {
+        if (IsValidDiscountAmount(amount))
+            this.Price -= amount;
+    }
+    public void Show()
+    {
+        Console.WriteLine($"" +
+            $"\nproduct summary:" +
+            $"\nproduct name:{Name}" +
+            $"\nprice: {Price}" +
+            $"\nID: {Id}");
+    }
+    public static void ShowTotalProduct()
+    {
+        Console.WriteLine($"the number of product is: {_count}");
+    }
+    private bool IsValidDiscountAmount(decimal amount)
+    {
+        if (amount > 0 && amount <= Price)
+            return true;
+        else 
+        {
+            Console.WriteLine("amount of discount is not valid");
+            return false;
+        }
+
+    }
+
+}
--- project/HelloWorld/Store/Program.cs
+++ project/HelloWorld/Store/Program.cs
@@ -6,22 +6,22 @@ namespace Store
     {
         static void Main(string[] args)
         {
-            //Product p1 = new Product("mobile");
-            //Product p2 = new Product("mobile",100);
-            //Product p3 = new Product("mobile") {Name="minote"};
-            //p1.ApplyDiscount(10);
-            //p2.ApplyDiscountByAmount(10);
-            //p1.Show();
-            //p2.Show();
-            //p3.Show();
-            //Product.ShowTotalProduct();
+            Product p1 = new Product("mobile");
+            Product p2 = new Product("mobile", 100);
+            Product p3 = new Product("mobile") { Name = "minote" };
+            p1.ApplyDiscount(10);
+            p2.ApplyDiscountByAmount(10);
+            p1.Show();
+            p2.Show();
+            p3.Show();
+            Product.ShowTotalProduct();
 
-            Employee e1 = new("hamid", 500);
-            Manager m1 = new Manager("mohsen", 2000, 5);
-            SalesPerson s1 = new SalesPerson("arash", 2500, 100000);
-            e1.ShowInfo();
-            m1.ShowInfo();
-            s1.ShowInfo();
+            //Employee e1 = new("hamid", 500);
+            //Manager m1 = new Manager("mohsen", 2000, 5);
+            //SalesPerson s1 = new SalesPerson("arash", 2500, 100000);
+            //e1.ShowInfo();
+            //m1.ShowInfo();
+            //s1.ShowInfo();
 
 
 
