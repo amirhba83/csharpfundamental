8769fdc53d85d6363fb1472fb3a6f941f095a158
elia <amirhosseinbabaali83@gmail.com>
2026-09-01 13:34:50 +0330

    add encapsulation practice

---
+++ project/HelloWorld/Account/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Account;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        UserAccount user1 = new UserAccount("user1", "123456");
+        UserAccount user2 = new UserAccount("user2", "654321");
+        user1.Login("12222");
+        user1.Login("12444444");
+        user1.Login("14555555");
+        Console.WriteLine(user2.Login("4321"));
+        //Console.WriteLine(user2.Password);
+    }
+}
---
+++ project/HelloWorld/Account/UserAccount.cs
@@ -0,0 +1,71 @@
+﻿namespace Account;
+
+internal class UserAccount
+{
+    #region Fields
+    private string _username;
+    private string _password;
+    private int _loginAttempts;
+    private readonly string _accountId;
+    private static int _nextAccountId =0 ;
+    #endregion
+    #region Properties
+    public string Username
+    {
+        get;
+        private set;
+    }
+    public string AccountId
+    {
+        get { return _accountId; }
+    }
+    public string Password
+    {
+        private get { return  _password; }
+        set
+        {
+            if (value.Length >= 6)
+            {
+                _password = value;
+            }
+            else
+            {
+                Console.WriteLine("password must be at least 6 characters long");
+            }
+        }
+    }
+    #endregion
+    #region Constructors
+    public UserAccount(string username, string password)
+    {
+        Username = username;
+        Password = password;
+        _loginAttempts = 0;
+        _accountId = "ACC-"+_nextAccountId;
+        _nextAccountId ++;
+    }
+    #endregion
+    #region methods
+    public bool Login (string password)
+    {
+        if (_password == password)
+        {
+            _loginAttempts = 0;
+            return true;
+        }
+        else 
+        {
+            _loginAttempts++;
+            if (_loginAttempts >= 3)
+            {
+                Console.WriteLine("Account locked due to too many failed login attempts.");
+            }
+            return false;
+        }
+        
+            
+        
+    }
+    #endregion
+
+}
